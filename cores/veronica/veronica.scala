ede9461b7bdf1af632565ee6684bdfe152a3ffed
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-08-20 19:35:07 -0400

    first addition of rom file gen

--- cores/veronica/veronica.scala
+++ cores/veronica/veronica.scala
@@ -1,5 +1,7 @@
 package vexriscv.afrl
 
+import java.nio.{ByteBuffer, ByteOrder}
+
 import spinal.core._
 import spinal.lib._
 import spinal.lib.bus.amba3.apb._
@@ -56,6 +58,17 @@ object configBUS {
     addressWidth = getAxi4Config().addressWidth,
     dataWidth = getAxi4Config().dataWidth
   )
+  
+  def getAxi4RamConfig(dataWidth : Int, byteCount : BigInt,idWidth : Int) = Axi4Config(
+    addressWidth = log2Up(byteCount/(dataWidth/8)),
+    dataWidth = dataWidth,
+    idWidth = idWidth,
+    useLock = false,
+    useRegion = false,
+    useCache = false,
+    useProt = false,
+    useQos = false
+  )
 }
 
 //axi lite connection
@@ -85,15 +98,15 @@ case class Axi4SharedToAxi4(axiConfig : Axi4Config) extends Component{
   io.output <> io.input.toAxi4()
 }
 
-case class Axi4Clint(hartCount : Int, bufferTime : Boolean = false) extends Component{
+case class AxiLite4Clint(hartCount : Int, bufferTime : Boolean = false) extends Component{
   val io = new Bundle {
-    val bus = slave(Axi4(configBUS.getAxi4Config()))
+    val bus = slave(AxiLite4(configBUS.getAxiLite4Config()))
     val timerInterrupt = out Bits(hartCount bits)
     val softwareInterrupt = out Bits(hartCount bits)
     val time = out UInt(64 bits)
   }
 
-  val factory = new Axi4SlaveFactory(io.bus)
+  val factory = new AxiLite4SlaveFactory(io.bus)
   val logic = Clint(0 until hartCount)
   logic.driveFrom(factory, bufferTime)
 
@@ -105,13 +118,13 @@ case class Axi4Clint(hartCount : Int, bufferTime : Boolean = false) extends Comp
   io.time := logic.time
 }
 
-class Axi4Plic(sourceCount : Int, targetCount : Int) extends Component{
+class AxiLite4Plic(sourceCount : Int, targetCount : Int) extends Component{
   val priorityWidth = 2
   val plicMapping = PlicMapping.sifive
   import plicMapping._
-  
+
   val io = new Bundle {
-    val bus = slave(Axi4(configBUS.getAxi4Config()))
+    val bus = slave(AxiLite4(configBUS.getAxiLite4Config()))
     val sources = in Bits (sourceCount bits)
     val targets = out Bits (targetCount bits)
   }
@@ -130,95 +143,66 @@ class Axi4Plic(sourceCount : Int, targetCount : Int) extends Component{
 
   io.targets := targets.map(_.iep).asBits
 
-  val bus = new Axi4SlaveFactory(io.bus)
+  val bus = new AxiLite4SlaveFactory(io.bus)
   val mapping = PlicMapper(bus, plicMapping)(
     gateways = gateways,
     targets = targets
   )
 }
 
-// class Axi4SharedRom(onChipRamBinFile : String) extends Component{
-//   import java.nio.file.{Files, Paths}
-//   val byteArray = Files.readAllBytes(Paths.get(onChipRamBinFile))
-//   val wordCount = (byteArray.length+3)/4
-//   val buffer = ByteBuffer.wrap(Files.readAllBytes(Paths.get(onChipRamBinFile))).order(ByteOrder.LITTLE_ENDIAN);
-//   val wordArray = (0 until wordCount).map(i => {
-//     val v = buffer.getInt
-//     if(v < 0)  BigInt(v.toLong & 0xFFFFFFFFl) else  BigInt(v)
-//   })
-// 
-//   val io = new Bundle{
-//     val bus = slave(Apb3(22,configBUS.getAxi4Config().dataWidth))
-//   }
-//   val rom = Mem(Bits(32 bits), wordCount) initBigInt(wordArray)
-// //  io.bus.PRDATA := rom.readSync(io.apb.PADDR >> 2)
-//   io.bus.PRDATA := rom.readAsync(RegNext(io.apb.PADDR >> 2))
-//   io.bus.PREADY := True
-// }
+case class Axi4SharedOnChipInitRam(onChipRamBinFile : String, dataWidth : Int, byteCount : BigInt, idWidth : Int, arwStage : Boolean = false) extends Component{
+  val axiConfig = configBUS.getAxi4RamConfig(dataWidth,byteCount,idWidth)
+  
+  val io = new Bundle {
+    val axi = slave(Axi4Shared(axiConfig))
+  }
+  
+  import java.nio.file.{Files, Paths}
+  val wordCount = byteCount / axiConfig.bytePerWord
+  val buffer = ByteBuffer.wrap(Files.readAllBytes(Paths.get(onChipRamBinFile))).order(ByteOrder.LITTLE_ENDIAN);
+  val wordArray = (0 until wordCount.toInt).map(i => {
+    if(buffer.hasRemaining())
+    {
+      val v = buffer.getInt
+      if(v < 0)  BigInt(v.toLong & 0xFFFFFFFFl) else  BigInt(v)
+    } 
+    else
+    {
+      BigInt(0)
+    }
+  })
 
-// case class Axi4ReadOnlyCC(axiConfig : Axi4Config,
-//                           inputCd : ClockDomain,
-//                           outputCd : ClockDomain,
-//                           arFifoSize : Int,
-//                           rFifoSize : Int) extends Component{
-//   val io = new Bundle {
-//     val input = slave(Axi4ReadOnly(axiConfig))
-//     val output = master(Axi4ReadOnly(axiConfig))
-//   }
-// 
-//   io.output.ar << io.input.ar.queue(arFifoSize, inputCd, outputCd)
-//   io.input.r   << io.output.r.queue(rFifoSize, outputCd, inputCd)
-// }
+  val ram = Mem(axiConfig.dataType,wordCount.toInt) initBigInt(wordArray)
+  val wordRange = log2Up(wordCount) + log2Up(axiConfig.bytePerWord)-1 downto log2Up(axiConfig.bytePerWord)
 
-// need to remove write connections
-// class Axi4SharedOnChipRom(dataWidth : Int, byteCount : BigInt, idWidth : Int, arwStage : Boolean = false, onChipRamBinFile : String) extends Component{
-//   val axiConfig = Axi4SharedOnChipRam.getAxiConfig(dataWidth,byteCount,idWidth)
-// 
-//   import java.nio.file.{Files, Paths}
-//   val byteArray = Files.readAllBytes(Paths.get(onChipRamBinFile))
-//   val wordCount = (byteArray.length+3)/4
-//   val buffer = ByteBuffer.wrap(Files.readAllBytes(Paths.get(onChipRamBinFile))).order(ByteOrder.LITTLE_ENDIAN);
-//   val wordArray = (0 until wordCount).map(i => {
-//     val v = buffer.getInt
-//     if(v < 0)  BigInt(v.toLong & 0xFFFFFFFFl) else  BigInt(v)
-//   })
-//   
-//   val io = new Bundle {
-//     val axi = slave(Axi4Shared(axiConfig))
-//   }
-// 
-//   val wordCount = byteCount / axiConfig.bytePerWord
-// //   val ram = Mem(axiConfig.dataType,wordCount.toInt)
-//   val rom = Mem(axiConfig.dataType, wordCount.toInt) initBigInt(wordArray)
-//   val wordRange = log2Up(wordCount) + log2Up(axiConfig.bytePerWord)-1 downto log2Up(axiConfig.bytePerWord)
-// 
-//   val arw = if(arwStage) io.axi.arw.s2mPipe().unburstify.m2sPipe() else io.axi.arw.unburstify
-//   val stage0 = arw.haltWhen(arw.write && !io.axi.writeData.valid)
-//   io.axi.readRsp.data := ram.readWriteSync(
-//     address = stage0.addr(axiConfig.wordRange).resized,
-//     data = io.axi.writeData.data,
-//     enable = stage0.fire,
-//     write = stage0.write,
-//     mask = io.axi.writeData.strb
-//   )
-//   io.axi.writeData.ready :=  arw.valid && arw.write  && stage0.ready
-// 
-//   val stage1 = stage0.stage()
-//   stage1.ready := (io.axi.readRsp.ready && !stage1.write) || ((io.axi.writeRsp.ready || ! stage1.last) && stage1.write)
-// 
-//   io.axi.readRsp.valid  := stage1.valid && !stage1.write
-//   io.axi.readRsp.id  := stage1.id
-//   io.axi.readRsp.last := stage1.last
-//   io.axi.readRsp.setOKAY()
-//   if(axiConfig.useRUser) io.axi.readRsp.user  := stage1.user
-// 
-//   io.axi.writeRsp.valid := stage1.valid &&  stage1.write && stage1.last
-//   io.axi.writeRsp.setSLVERR()
-//   io.axi.writeRsp.id := stage1.id
-//   if(axiConfig.useBUser) io.axi.writeRsp.user := stage1.user
-// 
-//   io.axi.arw.ready.noBackendCombMerge //Verilator perf
-// }
+  val arw = if(arwStage) io.axi.arw.s2mPipe().unburstify.m2sPipe() else io.axi.arw.unburstify
+  val stage0 = arw.haltWhen(arw.write && !io.axi.writeData.valid)
+  io.axi.readRsp.data := ram.readWriteSync(
+    address = stage0.addr(axiConfig.wordRange).resized,
+    data = io.axi.writeData.data,
+    enable = stage0.fire,
+    write = stage0.write,
+    mask = io.axi.writeData.strb
+  )
+  
+  io.axi.writeData.ready :=  arw.valid && arw.write  && stage0.ready
+
+  val stage1 = stage0.stage()
+  stage1.ready := (io.axi.readRsp.ready && !stage1.write) || ((io.axi.writeRsp.ready || ! stage1.last) && stage1.write)
+
+  io.axi.readRsp.valid  := stage1.valid && !stage1.write
+  io.axi.readRsp.id  := stage1.id
+  io.axi.readRsp.last := stage1.last
+  io.axi.readRsp.setOKAY()
+  if(axiConfig.useRUser) io.axi.readRsp.user  := stage1.user
+
+  io.axi.writeRsp.valid := stage1.valid &&  stage1.write && stage1.last
+  io.axi.writeRsp.setOKAY()
+  io.axi.writeRsp.id := stage1.id
+  if(axiConfig.useBUser) io.axi.writeRsp.user := stage1.user
+
+  io.axi.arw.ready.noBackendCombMerge //Verilator perf
+}
 
 sealed trait jtag_type
 object jtag_type {
@@ -229,9 +213,9 @@ object jtag_type {
 
 case class VeronicaConfig(  jtag_select : jtag_type,
                             ddr_size    : BigInt = 1 GB,
-                            ram_size    : BigInt = 16 kB,
-                            rom_size    : BigInt = 16 kB,
-                            rom_name    : String = "zebbs.bin",
+                            ram_size    : BigInt = 8 kB,
+                            rom_size    : BigInt = 8 kB,
+                            rom_name    : String = "rom/zebbs.bin",
                             cpuPlugins  : ArrayBuffer[Plugin[VexRiscv]])
 
 object VeronicaConfig{
@@ -322,37 +306,6 @@ object VeronicaConfig{
     //Replace static memory translator with pmp translator
     config.cpuPlugins(config.cpuPlugins.indexWhere(_.isInstanceOf[StaticMemoryTranslatorPlugin])) = new PmpPluginNapot(regions = 8,granularity = 8,ioRange = (x => x(31 downto 28) === 0x4 || x(31 downto 28) === 0x7 || x(31 downto 28) === 0x0))
 
-    //Replace standard CSR with secure CSR.
-//     config.cpuPlugins(config.cpuPlugins.indexWhere(_.isInstanceOf[CsrPlugin])) =
-//       new CsrPlugin(
-//           config = CsrPluginConfig(
-//             catchIllegalAccess  = true,
-//             mvendorid           = 0,
-//             marchid             = 0,
-//             mimpid              = 0,
-//             mhartid             = 0,
-//             misaExtensionsInit  = Riscv.misaToInt(s"imac"),
-//             misaAccess          = CsrAccess.READ_WRITE,
-//             mtvecAccess         = CsrAccess.READ_WRITE,
-//             xtvecModeGen        = true,
-//             mtvecInit           = 0x80000020l,
-//             mepcAccess          = CsrAccess.READ_WRITE,
-//             mscratchGen         = true,
-//             mcauseAccess        = CsrAccess.READ_WRITE,
-//             mbadaddrAccess      = CsrAccess.READ_WRITE,
-//             mcycleAccess        = CsrAccess.READ_WRITE,
-//             minstretAccess      = CsrAccess.READ_WRITE,
-//             ucycleAccess        = CsrAccess.READ_ONLY,
-//             uinstretAccess      = CsrAccess.READ_ONLY,
-//             wfiGenAsWait        = true,
-//             ecallGen            = true,
-//             userGen             = true,
-//             medelegAccess       = CsrAccess.READ_WRITE,
-//             midelegAccess       = CsrAccess.READ_WRITE,
-//             utimeAccess         = CsrAccess.READ_ONLY
-//           )
-//         )
-
     config
   }
 
@@ -418,10 +371,6 @@ object VeronicaConfig{
           earlyCacheHits = true
         )
       )
-
-    // floating point unit wthout double support
-    // config.cpuPlugins += new FpuPlugin(externalFpu = false,p = FpuParameter(withDouble = false))
-
     config
   }
 }
@@ -525,36 +474,37 @@ case class Veronica (val config: VeronicaConfig) extends Component {
     }
     
     val axiBUS = new ClockingArea(busClockDomain) {
-      val axi4acc  = Axi4SharedToAxiLite4(configBUS.getAxi4Config())
-      val axi4perf = Axi4SharedToAxiLite4(configBUS.getAxi4Config())
-      
-      val axi4clint = Axi4SharedToAxi4(configBUS.getAxi4Config())
-      val axi4plic  = Axi4SharedToAxi4(configBUS.getAxi4Config())
-      
-      val clint = new Axi4Clint(1)
-      val plic = new Axi4Plic(sourceCount = 127, targetCount = 2)
+      val clint = new AxiLite4Clint(1)
+      val plic = new AxiLite4Plic(sourceCount = 127, targetCount = 2)
       
       plic.io.sources  := BufferCC(io.irq >> 1)
       
+      val axi4acc  = Axi4SharedToAxiLite4(configBUS.getAxi4Config())
+      val axi4perf = Axi4SharedToAxiLite4(configBUS.getAxi4Config())
+      
+      val axi4clint = Axi4SharedToAxiLite4(configBUS.getAxi4Config())
+      val axi4plic  = Axi4SharedToAxiLite4(configBUS.getAxi4Config())
+      
+      val rom = Axi4SharedOnChipInitRam(
+        onChipRamBinFile = config.rom_name,
+        dataWidth = configBUS.getAxi4Config().dataWidth,
+        byteCount = config.rom_size,
+        idWidth   = configBUS.getAxi4Config().idWidth,
+        arwStage  = true
+      )
+
       val ram = Axi4SharedOnChipRam(
         dataWidth = configBUS.getAxi4Config().dataWidth,
         byteCount = config.ram_size,
         idWidth   = configBUS.getAxi4Config().idWidth,
         arwStage  = true
       )
-      
-//       val rom = Axi4SharedOnChipRom(
-//         dataWidth = configBUS.getAxi4Config().dataWidth,
-//         byteCount = config.rom_size,
-//         idWidth   = configBUS.getAxi4Config().idWidth,
-//         arwStage  = true,
-//         config.rom_name
-//       )
     }
     
     val axi4perfCC  = Axi4SharedCC(configBUS.getAxi4Config(), cpuClockDomain, busClockDomain, 8, 8, 8, 8)
     val axi4accCC   = Axi4SharedCC(configBUS.getAxi4Config(), cpuClockDomain, busClockDomain, 8, 8, 8, 8)
     val axi4ramCC   = Axi4SharedCC(axiBUS.ram.axiConfig, cpuClockDomain, busClockDomain, 8, 8, 8, 8)
+    val axi4romCC   = Axi4SharedCC(axiBUS.rom.axiConfig, cpuClockDomain, busClockDomain, 8, 8, 8, 8)
     val axi4mbusCC  = Axi4SharedCC(configBUS.getAxi4Config(), cpuClockDomain, ddrClockDomain, 8, 8, 8, 8)
     val axi4clintCC = Axi4SharedCC(configBUS.getAxi4Config(), cpuClockDomain, busClockDomain, 8, 8, 8, 8)
     val axi4plicCC  = Axi4SharedCC(configBUS.getAxi4Config(), cpuClockDomain, busClockDomain, 8, 8, 8, 8)
@@ -610,20 +560,19 @@ case class Veronica (val config: VeronicaConfig) extends Component {
 
       val axiCrossbar = Axi4CrossbarFactory()
 
-//       rom.io.axi          -> (0x20010000L,   config.rom_size) //16 KiB
       axiCrossbar.addSlaves(
         axi4accCC.io.input        -> (0x70000000L,   256 MB),
         axi4perfCC.io.input       -> (0x40000000L,   256 MB),
-        axi4mbusCC.io.input       -> (0x90000000L,   config.ddr_size), //0x80000000L
+        axi4mbusCC.io.input       -> (0x80000000L,   config.ddr_size),
         axi4clintCC.io.input      -> (0x02000000L,    64 kB),
         axi4plicCC.io.input       -> (0x0C000000L,     4 MB),
-        axi4ramCC.io.input        -> (0x80000000L,   config.ram_size) //0x01800000L PER E31 ITIM //0x08000000L PER E310?
+        axi4ramCC.io.input        -> (0x08000000L,   config.ram_size),
+        axi4romCC.io.input        -> (0x20010000L,   config.rom_size)
       )
 
-//       rom.io.axi
       axiCrossbar.addConnections(
-        core.iBus -> List(axi4ramCC.io.input, axi4mbusCC.io.input),
-        core.dBus -> List(axi4ramCC.io.input, axi4mbusCC.io.input, axi4clintCC.io.input, axi4plicCC.io.input, axi4accCC.io.input, axi4perfCC.io.input) //apbBridge.io.axi,
+        core.iBus -> List(axi4ramCC.io.input, axi4romCC.io.input, axi4mbusCC.io.input),
+        core.dBus -> List(axi4ramCC.io.input, axi4romCC.io.input, axi4mbusCC.io.input, axi4clintCC.io.input, axi4plicCC.io.input, axi4accCC.io.input, axi4perfCC.io.input)
       )
 
       axiCrossbar.addPipelining(core.dBus)((cpu,crossbar) => {
@@ -647,6 +596,7 @@ case class Veronica (val config: VeronicaConfig) extends Component {
     
     axiBUS.clint.io.bus   <> axiBUS.axi4clint.io.output
     axiBUS.plic.io.bus    <> axiBUS.axi4plic.io.output
+    axiBUS.rom.io.axi     <> axi4romCC.io.output
     axiBUS.ram.io.axi     <> axi4ramCC.io.output
     
     axiBUS.axi4acc.io.input  <> axi4accCC.io.output
